01505fe5d35ceb552f9586b6b8be8f97c7644155
valentinohansenn <valentinohansen14@gmail.com>
2024-06-11 17:59:30 +1000

    initial commit

---
+++ app/(root)/transformation/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const TransformationPage = () => {
+	return <div>Transformation Page</div>
+}
+
+export default TransformationPage
---
+++ app/(root)/transformation/[id]/update/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const UpdateTransformation = () => {
+	return <div>Update Transformation</div>
+}
+
+export default UpdateTransformation
