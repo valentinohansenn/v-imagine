765c9df8d2541a7a94c072b2a550af77cad24ac8
valentinohansenn <valentinohansen14@gmail.com>
2024-06-11 18:35:45 +1000

    added authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from "@clerk/nextjs"
+
 const Home = () => {
-	return <div>Home</div>
+	return (
+		<div>
+			<p>Home</p>
+			<UserButton afterSignOutUrl="/" />
+		</div>
+	)
 }
 
 export default Home
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { IBM_Plex_Sans } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
 
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/nextjs"
+
 const IBMPlex = IBM_Plex_Sans({
 	subsets: ["latin"],
 	weight: ["400", "500", "600", "700"],
@@ -20,10 +23,18 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang="en">
-			<body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-				{children}
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+					<SignedOut>
+						<SignInButton />
+					</SignedOut>
+					<SignedIn>
+						<UserButton />
+					</SignedIn>
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
 	)
 }
