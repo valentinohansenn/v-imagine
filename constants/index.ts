40087642092bfd5efa9c295fc98dce67afad1b75
valentinohansenn <valentinohansen14@gmail.com>
2024-06-14 15:42:43 +1000

    implement clerk webhooks

--- constants/index.ts
+++ constants/index.ts
@@ -1,32 +1,32 @@
-export const navigationLinks = [
+export const navLinks = [
 	{
 		label: "Home",
 		route: "/",
 		icon: "/assets/icons/home.svg",
 	},
 	{
-		label: "Restore Images",
-		route: "/transformation/add/restore",
+		label: "Image Restore",
+		route: "/transformations/add/restore",
 		icon: "/assets/icons/image.svg",
 	},
 	{
 		label: "Generative Fill",
-		route: "/transformation/add/fill",
+		route: "/transformations/add/fill",
 		icon: "/assets/icons/stars.svg",
 	},
 	{
-		label: "Remove Images",
-		route: "/transformation/add/remove",
+		label: "Object Remove",
+		route: "/transformations/add/remove",
 		icon: "/assets/icons/scan.svg",
 	},
 	{
-		label: "Recolor Images",
-		route: "/transformation/add/recolor",
+		label: "Object Recolor",
+		route: "/transformations/add/recolor",
 		icon: "/assets/icons/filter.svg",
 	},
 	{
-		label: "Remove Background",
-		route: "/transformation/add/removeBackground",
+		label: "Background Remove",
+		route: "/transformations/add/removeBackground",
 		icon: "/assets/icons/camera.svg",
 	},
 	{
@@ -35,84 +35,84 @@ export const navigationLinks = [
 		icon: "/assets/icons/profile.svg",
 	},
 	{
-		label: "Buy Tokens",
-		route: "/tokens",
+		label: "Buy Credits",
+		route: "/credits",
 		icon: "/assets/icons/bag.svg",
 	},
 ]
 
-export const subscriptionPlan = [
+export const plans = [
 	{
-		id: "1",
+		_id: 1,
 		name: "Free",
 		icon: "/assets/icons/free-plan.svg",
 		price: 0,
-		tokens: 20,
+		credits: 20,
 		inclusions: [
 			{
-				label: "20 Free Tokens",
+				label: "20 Free Credits",
 				isIncluded: true,
 			},
 			{
-				label: "Basic Access to Free Services",
+				label: "Basic Access to Services",
 				isIncluded: true,
-			},
-			{
-				label: "Priority Updates",
-				isIncluded: false,
 			},
 			{
 				label: "Priority Customer Support",
 				isIncluded: false,
 			},
+			{
+				label: "Priority Updates",
+				isIncluded: false,
+			},
 		],
 	},
 	{
-		id: "2",
-		name: "Plus",
+		_id: 2,
+		name: "Pro Package",
 		icon: "/assets/icons/free-plan.svg",
-		price: 49,
-		tokens: 120,
+		price: 40,
+		credits: 120,
 		inclusions: [
 			{
-				label: "120 Tokens",
+				label: "120 Credits",
 				isIncluded: true,
 			},
 			{
-				label: "Full Access to All Services",
+				label: "Full Access to Services",
+				isIncluded: true,
+			},
+			{
+				label: "Priority Customer Support",
 				isIncluded: true,
 			},
 			{
 				label: "Priority Updates",
 				isIncluded: false,
 			},
-			{
-				label: "Priority Customer Support",
-				isIncluded: true,
-			},
 		],
 	},
 	{
-		id: "3",
-		name: "Pro",
+		_id: 3,
+		name: "Premium Package",
 		icon: "/assets/icons/free-plan.svg",
 		price: 199,
-		tokens: 2000,
+		credits: 2000,
 		inclusions: [
 			{
-				label: "2000 Tokens",
+				label: "2000 Credits",
 				isIncluded: true,
 			},
 			{
-				label: "Full Access to All Services",
-				isIncluded: true,
-			},
-			{
-				label: "Priority Updates",
+				label: "Full Access to Services",
 				isIncluded: true,
 			},
 			{
 				label: "Priority Customer Support",
+				isIncluded: true,
+			},
+			{
+				label: "Priority Updates",
 				isIncluded: true,
 			},
 		],
@@ -123,29 +123,71 @@ export const transformationTypes = {
 	restore: {
 		type: "restore",
 		title: "Restore Image",
-		subtitle: "Enhance image by removing noise and imperfections",
+		subTitle: "Refine images by removing noise and imperfections",
 		config: { restore: true },
-		icon: "images.svg",
+		icon: "image.svg",
+	},
+	removeBackground: {
+		type: "removeBackground",
+		title: "Background Remove",
+		subTitle: "Removes the background of the image using AI",
+		config: { removeBackground: true },
+		icon: "camera.svg",
 	},
 	fill: {
 		type: "fill",
 		title: "Generative Fill",
-		subtitle: "Enhance image's dimension by utilizing AI Outpainting Image",
+		subTitle: "Enhance an image's dimensions using AI outpainting",
 		config: { fillBackground: true },
 		icon: "stars.svg",
 	},
 	remove: {
 		type: "remove",
-		title: "Remove Image",
-		subtitle: "Eliminate unnecessary objects from image",
-		config: { prompt: "", removeShadow: true, multiple: true },
+		title: "Object Remove",
+		subTitle: "Identify and eliminate objects from images",
+		config: {
+			remove: { prompt: "", removeShadow: true, multiple: true },
+		},
 		icon: "scan.svg",
 	},
 	recolor: {
-		type: "restore",
-		title: "Restore Image",
-		subtitle: "Enhance image by removing noise and imperfections",
-		config: { restore: true },
-		icon: "images.svg",
+		type: "recolor",
+		title: "Object Recolor",
+		subTitle: "Identify and recolor objects from the image",
+		config: {
+			recolor: { prompt: "", to: "", multiple: true },
+		},
+		icon: "filter.svg",
 	},
 }
+
+export const aspectRatioOptions = {
+	"1:1": {
+		aspectRatio: "1:1",
+		label: "Square (1:1)",
+		width: 1000,
+		height: 1000,
+	},
+	"3:4": {
+		aspectRatio: "3:4",
+		label: "Standard Portrait (3:4)",
+		width: 1000,
+		height: 1334,
+	},
+	"9:16": {
+		aspectRatio: "9:16",
+		label: "Phone Portrait (9:16)",
+		width: 1000,
+		height: 1778,
+	},
+}
+
+export const defaultValues = {
+	title: "",
+	aspectRatio: "",
+	color: "",
+	prompt: "",
+	publicId: "",
+}
+
+export const creditFee = -1
