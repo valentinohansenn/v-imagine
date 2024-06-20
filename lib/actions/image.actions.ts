673fb26fe921346bba0001916eaaf7e87856ae38
valentinohansenn <valentinohansen14@gmail.com>
2024-06-21 00:25:51 +1000

    fix minor bug: typo

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -99,8 +99,8 @@ export async function getAllImages({
 
 		cloudinary.config({
 			cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-			api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-			api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+			api_key: process.env.CLOUDINARY_API_KEY,
+			api_secret: process.env.CLOUDINARY_API_SECRET,
 			secure: true,
 		})
 
